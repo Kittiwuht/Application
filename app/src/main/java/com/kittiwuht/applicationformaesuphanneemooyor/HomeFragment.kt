0e8fb2fa6e983cfcf61bf5a2143326fefd0b23d3
Kittiwuht Limpichai <kittiwuth0506@gmail.com>
2025-12-11 12:27:05 +0700

    first commit

--- app/src/main/java/com/kittiwuht/applicationformaesuphanneemooyor/HomeFragment.kt
+++ app/src/main/java/com/kittiwuht/applicationformaesuphanneemooyor/HomeFragment.kt
@@ -13,10 +13,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [activity_main.newInstance] factory method to
+ * Use the [HomeFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class activity_main : Fragment() {
+class HomeFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -34,7 +34,7 @@ class activity_main : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_activity_main, container, false)
+        return inflater.inflate(R.layout.fragment_home, container, false)
     }
 
     companion object {
@@ -44,12 +44,12 @@ class activity_main : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment activity_main.
+         * @return A new instance of fragment HomeFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            activity_main().apply {
+            HomeFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
--- app/src/main/java/com/kittiwuht/applicationformaesuphanneemooyor/MainActivity.kt
+++ app/src/main/java/com/kittiwuht/applicationformaesuphanneemooyor/MainActivity.kt
@@ -3,58 +3,39 @@ package com.kittiwuht.applicationformaesuphanneemooyor
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.Toolbar
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.NavigationUI
-import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
-import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
-    lateinit var navController: NavController
-    lateinit var appBarConfiguration: AppBarConfiguration
-    lateinit var mainToolbar: Toolbar
-    lateinit var mainDrawerLayout: DrawerLayout
-    lateinit var mainNavigationView: NavigationView
-    lateinit var mainBottomNavigationView: BottomNavigationView
+    // ประกาศตัวแปรสำหรับ Navigation
+    private lateinit var navController: NavController
+    private lateinit var mainBottomNavigationView: BottomNavigationView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+
+        // ตั้งค่า Layout หลักของ Activity
+        setContentView(R.layout.fragment_activity_main)
+
+        // จัดการ System Bars (status bar, navigation bar)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.homeFragment)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
-        mainToolbar = findViewById(R.id.toolbar)
-        mainDrawerLayout = findViewById(R.id.bottomNavigation)
-        mainNavigationView = findViewById(R.id.main_navigation_view)
+        // **สำคัญ**: หา NavHostFragment และดึง NavController ออกมา
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+
+        // เชื่อม Bottom Navigation กับ Navigation Controller
         mainBottomNavigationView = findViewById(R.id.main_bottom_navigation_view)
-
-        appBarConfiguration = AppBarConfiguration.Builder(
-            R.id.homeFragment,
-            R.id.profitFragment,
-            R.id.stockFragment
-        ).setOpenableLayout(mainDrawerLayout).build()
-
-        setSupportActionBar(mainToolbar)
-
-        setupActionBarWithNavController(navController,appBarConfiguration)
-
-        mainNavigationView.setupWithNavController(navController)
-
         mainBottomNavigationView.setupWithNavController(navController)
-
-    }
-    override fun onSupportNavigateUp(): Boolean {
-        return NavigationUI.navigateUp(navController,appBarConfiguration)
     }
 }
